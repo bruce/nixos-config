2cd8eaafbdcc2227aff5a73df4731ecfe00411ba
Bruce Williams <brwcodes@gmail.com>
2018-01-16 23:36:04 -0800

    Fix duplicate

--- cfg/wm.nix
+++ cfg/wm.nix
@@ -3,7 +3,6 @@
 
   services.xserver = {
     enable = true;
-    layout = "us";
     windowManager.xmonad.enable = true;
     windowManager.default = "xmonad";
     desktopManager.xterm.enable = false;
