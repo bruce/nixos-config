9d604db39b8247c558fd67d901c84fbe61794f4d
Bruce Williams <brwcodes@gmail.com>
2018-01-18 23:52:56 -0800

    Set xmonad

--- cfg/desktop.nix
+++ cfg/desktop.nix
@@ -1,17 +1,24 @@
 { config, lib, pkgs, ... }:
 
 {
-  services = {
-    xserver = {
-      enable = true;
-      displayManager.sddm.enable = true;
-      desktopManager.plasma5.enable = true;
+  services.xserver = {
+    enable = true;
+    windowManager.xmonad.enable = true;
+    windowManager.default = "xmonad";
+    desktopManager.xterm.enable = true;
+    desktopManager.default = "none";
+    displayManager = {
+      slim = {
+	      enable = true;
+	      defaultUser = "bruce";
+      };
     };
   };
 
   environment.systemPackages = with pkgs; [
-    kdeApplications.ark
-    kdeApplications.gwenview
-    kdeApplications.okular
+    haskellPackages.xmobar
+    haskellPackages.xmonad
+    haskellPackages.xmonad-contrib
+    haskellPackages.xmonad-extras
   ];
 }
