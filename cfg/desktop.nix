fc75f1d49c772e891710fecc5f3b8b47492a3e00
Bruce Williams <brwcodes@gmail.com>
2018-01-22 17:56:20 -0800

    Add urxvt

--- cfg/desktop.nix
+++ cfg/desktop.nix
@@ -16,10 +16,22 @@
   };
 
   environment.systemPackages = with pkgs; [
+    rxvt_unicode
     dmenu
     haskellPackages.xmobar
     haskellPackages.xmonad
     haskellPackages.xmonad-contrib
     haskellPackages.xmonad-extras
   ];
+
+  systemd.user.services."urxvtd" = {
+    enable = true;
+    description = "rxvt unicode daemon";
+    wantedBy = [ "default.target" ];
+    path = [ pkgs.rxvt_unicode ];
+    serviceConfig.Restart = "always";
+    serviceConfig.RestartSec = 2;
+    serviceConfig.ExecStart = "${pkgs.rxvt_unicode}/bin/urxvtd -q -o";
+  };
+
 }
