5f2f6485111083804c71311776077e4ff7719877
Bruce Williams <brwcodes@gmail.com>
2018-02-06 16:57:58 -0800

    Update authentication

--- cfg/databases.nix
+++ cfg/databases.nix
@@ -5,6 +5,9 @@
     enable = true;
     package = pkgs.postgresql95;
     extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql95; }).v_2_3_1 ];
+    authentication = lib.mkForce ''
+      host all all localhost trust
+    '';
   };
   services.redis = {
     enable = true;
