46cd59696157d269b677f399db1d5dc064b484b1
Bruce Williams <brwcodes@gmail.com>
2018-02-06 17:15:49 -0800

    Add localhost entry

--- cfg/databases.nix
+++ cfg/databases.nix
@@ -5,6 +5,13 @@
     enable = true;
     package = pkgs.postgresql95;
     extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql95; }).v_2_3_1 ];
+    authentication = lib.mkForce ''
+      # Generated file; do not edit!
+      local all all              ident ${optionalString pre84 "sameuser"}
+      host  all all localhost    trust
+      host  all all 127.0.0.1/32 md5
+      host  all all ::1/128      md5
+    '';
   };
   services.redis = {
     enable = true;
