4cd41fe3662db9ee1041729896b18e8ef9e17e73
Bruce Williams <brwcodes@gmail.com>
2018-02-06 17:16:55 -0800

    Add sameuser

--- cfg/databases.nix
+++ cfg/databases.nix
@@ -7,7 +7,7 @@
     extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql95; }).v_2_3_1 ];
     authentication = lib.mkForce ''
       # Generated file; do not edit!
-      local all all              ident ${optionalString pre84 "sameuser"}
+      local all all              ident sameuser
       host  all all localhost    trust
       host  all all 127.0.0.1/32 md5
       host  all all ::1/128      md5
