68e7b7f01f3ac6ad0f45dfef4e97e5eed0c330f2
Bruce Williams <brwcodes@gmail.com>
2018-02-06 16:19:25 -0800

    Update postgres version

--- cfg/databases.nix
+++ cfg/databases.nix
@@ -3,6 +3,7 @@
 
   services.postgresql = {
     enable = true;
+    package = pkgs.postgresql95;
     extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql95; }).v_2_3_1 ];
   };
   services.redis = {
