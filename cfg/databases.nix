a38f62b47116bb6f3951cdc094aa4cead65de379
Bruce Williams <brwcodes@gmail.com>
2018-02-06 17:12:16 -0800

    Don't override authentication

--- cfg/databases.nix
+++ cfg/databases.nix
@@ -5,9 +5,6 @@
     enable = true;
     package = pkgs.postgresql95;
     extraPlugins = [ (pkgs.postgis.override { postgresql = pkgs.postgresql95; }).v_2_3_1 ];
-    authentication = lib.mkForce ''
-      host all all localhost trust
-    '';
   };
   services.redis = {
     enable = true;
